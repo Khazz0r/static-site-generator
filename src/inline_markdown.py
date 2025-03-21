c14a7d7ec7b30e39bea349c342df73decd2f8501
Khazz0r <kaw3198@gmail.com>
2025-03-21 17:17:31 -0400

    add inline_markdown logic for creating new nodes and add unit tests for it

---
+++ src/inline_markdown.py
@@ -0,0 +1,23 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if delimiter == "**" or delimiter == "_" or delimiter == "`":
+        new_nodes = []
+
+        for node in old_nodes:
+            if node.text_type != TextType.NORMAL:
+                new_nodes.append(node)
+                continue
+            split_text = node.text.split(delimiter)
+            for i in range(len(split_text)):
+                if split_text[i] == "":
+                    continue
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(split_text[i], TextType.NORMAL))
+                else:
+                    new_nodes.append(TextNode(split_text[i], text_type))
+                        
+        return new_nodes
+    
+    else:
+        raise Exception("delimiter selected is not a valid delimiter")
