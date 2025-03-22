502b02d8088115075064ad70c2084b3ede9b1ee1
Khazz0r <kaw3198@gmail.com>
2025-03-22 12:46:44 -0400

    add to main.py a recursive function that copies all directories/files from static directory to public directory

--- src/main.py
+++ src/main.py
@@ -1,7 +1,31 @@
+import os, shutil
 from textnode import TextNode
 
 def main():
-    text = TextNode("This is some anchor text", "link", "https://www.boot.dev")
-    print(text)
+    copy_static_to_public("static/", "public/")
+
+
+def copy_static_to_public(static_dir, public_dir):
+    if not os.path.exists(static_dir):
+        raise FileNotFoundError(f"Static directory '{static_dir}' does not exist.")
+    
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir)
+    os.mkdir(public_dir)
+    
+    recursive_file_dir_copy(static_dir, public_dir)
+
+
+def recursive_file_dir_copy(static_src, public_dest):
+    for item in os.listdir(static_src):
+        src_path = os.path.join(static_src, item)
+        dest_path = os.path.join(public_dest, item)
+        
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+        else:
+            os.mkdir(dest_path)
+            recursive_file_dir_copy(src_path, dest_path)
+
 
 main()
